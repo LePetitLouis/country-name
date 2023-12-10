5a2172af6196c77fe1e2c4485de257f8b65a5297
Louis Poupat <louispoupat@sfr.fr>
2023-12-10 10:29:32 +0400

    [fix] build

--- src/components/capitale/SidebarCapitale.vue
+++ src/components/capitale/SidebarCapitale.vue
@@ -19,12 +19,12 @@
             class="transition-all duration-250 h-8 opacity-100 flex items-center"
           >
             <img
-              :src="`/icons/${getDetailsOfCapitale(capitale).code.toLocaleLowerCase()}.svg`"
-              :alt="getDetailsOfCapitale(capitale).code"
+              :src="`/icons/${getDetailsOfCapitale(capitale)?.code.toLocaleLowerCase()}.svg`"
+              :alt="getDetailsOfCapitale(capitale)?.code"
               class="w-5 h-5 -mr-0.5"
             />
             <span class="ml-2.5 max-w-md truncate text-stone-800">
-              {{ getDetailsOfCapitale(capitale).capitale }} - {{ getDetailsOfCapitale(capitale).nameCountry }}
+              {{ getDetailsOfCapitale(capitale)?.capitale }} - {{ getDetailsOfCapitale(capitale)?.nameCountry }}
             </span>
           </li>
         </template>
@@ -55,6 +55,6 @@ const getDetailsOfCapitale = (capitale: string) => {
     item.capitale.toLocaleLowerCase().includes(capitale.toLocaleLowerCase())
   );
 
-  return foundCapitale || null;
+  return foundCapitale;
 };
 </script>
